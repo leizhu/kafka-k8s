9c1c291037897308ba05867c62dd6619c2c373dc
Lei Zhu <lei.zhu.1017@gmail.com>
2017-07-07 10:32:58 +0800

    remove kafka-monitor

--- k8s/kafka-monitor/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-echo "Starting kafka-monitor"
-exec java -cp /opt/kafka-monitor/KafkaOffsetMonitor-assembly-0.2.1.jar com.quantifind.kafka.offsetapp.OffsetGetterWeb --zk ${ZK_CONNECTION} --port ${MONITOR_PORT:-8080} --refresh ${REFRESH_SECONDS}.seconds --retain ${RETAIN_DAYS}.days --dbName /opt/kafka-monitor/db/monitor 
